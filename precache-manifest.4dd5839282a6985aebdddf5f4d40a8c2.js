f9009da94c439f03973a4e6d50377d7844530f80
灬秋落丿 <1492354280@qq.com>
2024-12-13 14:03:56 +0800

    build

---
+++ precache-manifest.4dd5839282a6985aebdddf5f4d40a8c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d30d30a591874d0c438fbb304d662a5",
+    "url": "/flowpoints_ml_offline/index.html"
+  },
+  {
+    "revision": "0d74745aa8b1bd8bd9f0",
+    "url": "/flowpoints_ml_offline/static/css/main.3f241ef0.chunk.css"
+  },
+  {
+    "revision": "ec250a5170a7aa61aa75",
+    "url": "/flowpoints_ml_offline/static/js/2.480aab34.chunk.js"
+  },
+  {
+    "revision": "0d74745aa8b1bd8bd9f0",
+    "url": "/flowpoints_ml_offline/static/js/main.52635f6c.chunk.js"
+  },
+  {
+    "revision": "0b0ead3fed2412e37c92",
+    "url": "/flowpoints_ml_offline/static/js/runtime~main.f8d3fdb8.js"
+  }
+]);
